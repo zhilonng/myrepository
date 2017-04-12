8739fa84f8fbb790e761027b233b5d5ec7c2d6fc
dragon <huang_zhl@foxmail.com>
2017-04-12 17:52:39 +0800

    add function 'pay'

--- app.js
+++ app.js
@@ -2,9 +2,69 @@
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
+    var that = this;
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
+    wx.login({
+            success: function(res) {
+            //获取用户code，转发到我们的服务器，再在我们服务器与微信交互，获取openid
+                var code = res.code
+                wx.getUserInfo({
+                    success: function(userInfo) {
+                      console.log(userInfo)
+                        var encryptedData = userInfo.encryptedData
+                        var iv = userInfo.iv
+                        //我们服务器请求地址
+                        var url ='https://baby.mamid.cn/Api/Index/login'
+                        var userinfo = userInfo.userInfo
+                        var username = userinfo.nickName
+                        var useravatar =userinfo.avatarUrl
+                        var usersex=userinfo.gender
+                        wx.request({
+                            url: url,
+                            method: 'POST',
+                            data: {
+                                'iv':iv,
+                                'encryptedData':encryptedData,
+                                'code': code,
+                                'username':username,
+                                'useravatar':useravatar,
+                                'usersex':usersex
+                            },
+                            header: {
+                          "Content-Type": "application/x-www-form-urlencoded"
+                        },
+               success:function(response) {
+               //数据交互成功后，我们将服务器返回的数据写入全局变量与缓存中
+             console.log(response.data)
+             if(response.data.babyres.length == 0){
+               response.data.babyres.push({
+                 baby_avatar:'https://oj76c7lts.qnssl.com/baby_default_img.png',
+                 baby_birthday:"2017-03-01",
+                 baby_name:"请添加宝宝",
+                 baby_sex:"0",
+                 baby_id:'0'
+               })
+             }
+             //写入全局变量
+             that.globalData.userInfo = response.data
+             wx.hideToast()
+            //写入后，我们将跳转到主页
+            wx.redirectTo({
+              url: '../index/index',
+             })
+          },
+      failure: function(error) {
+               console.log(error)
+                            },
+                        })
+                    }
+                })
+            }
+        })
+
   },
   getUserInfo:function(cb){
     var that = this
--- pages/secondLevelPage/servicepurchasedetails/servicepurchasedetails.js
+++ pages/secondLevelPage/servicepurchasedetails/servicepurchasedetails.js
@@ -14,29 +14,59 @@ Page({
     // 页面初始化 options为页面跳转所带来的参数
     console.log(options)
     var that = this
-    if(options.index == 1){
-      //咨询服务
-      that.data.serviceName = "家庭保险-咨询服务"
-      //服务条款名称
-      that.data.serviceClauseName.push("服务内容")
-      that.data.serviceClauseName.push("服务形式")
-      that.data.serviceClauseName.push("服务时间")
-      //服务条款内容
-      that.data.serviceClauseContent.push("咨询保险相关内容")
-      that.data.serviceClauseContent.push("专家一对一微信咨询")
-      that.data.serviceClauseContent.push("7天")
-      //详情条款名称
-      that.data.detailsClauseName.push("有问必答")
-      that.data.detailsClauseName.push("认真建议")
-      that.data.detailsClauseName.push("专业解释")
-      //详情条款图片
-      that.data.detailsClauseImage.push("../../images/icon_6.png")
-      that.data.detailsClauseImage.push("../../images/icon_6.png")
-      that.data.detailsClauseImage.push("../../images/icon_6.png")
-      //详情条款内容
-      that.data.drtailsClauseContent.push("在服务有效期间，您可以通过妈咪研究院的微信公众号，随时随地向我们咨询任何保险相关问题，不限数量，不限主题。在上班时间（周一至周五9:00-18:00），我们会即时回复您的问题。非上班时间的留言，我们上班后尽可能第一时间给予回复。")
-       that.data.drtailsClauseContent.push("您也可以简单告诉我们您的家庭状况，我们会认真听，然后给出您和您家庭的保险配置大概方向。是的，一个正确的家庭保险配置方向，就可以避免大多数错误。")
-        that.data.drtailsClauseContent.push("我们还会比较生动的解释清楚一些比较难懂保险专属概念，教你从根本角度来认识保险核心问题，是的，这是我们的专业和特长。")
+    switch(options.index){
+      case "1":
+        //咨询服务
+        that.data.serviceName = "家庭保险-咨询服务"
+        //服务条款名称
+        that.data.serviceClauseName.push("服务内容")
+        that.data.serviceClauseName.push("服务形式")
+        that.data.serviceClauseName.push("服务时间")
+        //服务条款内容
+        that.data.serviceClauseContent.push("咨询保险相关内容")
+        that.data.serviceClauseContent.push("专家一对一微信咨询")
+        that.data.serviceClauseContent.push("7天")
+        //详情条款名称
+        that.data.detailsClauseName.push("有问必答")
+        that.data.detailsClauseName.push("认真建议")
+        that.data.detailsClauseName.push("专业解释")
+        //详情条款图片
+        that.data.detailsClauseImage.push("../../images/icon_6.png")
+        that.data.detailsClauseImage.push("../../images/icon_6.png")
+        that.data.detailsClauseImage.push("../../images/icon_6.png")
+        //详情条款内容
+        that.data.drtailsClauseContent.push("在服务有效期间，您可以通过妈咪研究院的微信公众号，随时随地向我们咨询任何保险相关问题，不限数量，不限主题。在上班时间（周一至周五9:00-18:00），我们会即时回复您的问题。非上班时间的留言，我们上班后尽可能第一时间给予回复。")
+        that.data.drtailsClauseContent.push("您也可以简单告诉我们您的家庭状况，我们会认真听，然后给出您和您家庭的保险配置大概方向。是的，一个正确的家庭保险配置方向，就可以避免大多数错误。")
+          that.data.drtailsClauseContent.push("我们还会比较生动的解释清楚一些比较难懂保险专属概念，教你从根本角度来认识保险核心问题，是的，这是我们的专业和特长。")
+          break;
+        case "2":
+          //咨询服务
+          that.data.serviceName = "家庭保险-托管服务"
+          //服务条款名称
+          that.data.serviceClauseName.push("服务内容")
+          that.data.serviceClauseName.push("服务形式")
+          that.data.serviceClauseName.push("服务时间")
+          //服务条款内容
+          that.data.serviceClauseContent.push("家庭保险配置、托管和管理")
+          that.data.serviceClauseContent.push("全家全程一体化服务")
+          that.data.serviceClauseContent.push("365天")
+          //详情条款名称
+          that.data.detailsClauseName.push("方案定制")
+          that.data.detailsClauseName.push("投保协助")
+          that.data.detailsClauseName.push("理赔协助")
+          that.data.detailsClauseName.push("有问必答")
+          //详情条款图片
+          that.data.detailsClauseImage.push("../../images/icon_6.png")
+          that.data.detailsClauseImage.push("../../images/icon_6.png")
+          that.data.detailsClauseImage.push("../../images/icon_6.png")
+          that.data.detailsClauseImage.push("../../images/icon_6.png")
+          //详情条款内容
+          that.data.drtailsClauseContent.push("✔按照您及您家庭的各项指标综合规划保险配置；\n✔帮您梳理以及分析现有保单，给出保单处理意见；\n✔从众多保险公司的各种保险产品中，精选高性比产品，配置适合您的保险方案。")
+          that.data.drtailsClauseContent.push("✔妈咪研究院提供一体化的投保协助；\n✔在多种投保渠道和投保方式中，提供更合适更方便的渠道和方式；\n✔帮您梳理健康告知情况，把关核保成功可能性，全程指导保单填写。\n")
+            that.data.drtailsClauseContent.push("✔首先从源头避免理赔难度，帮您明确购买保险的目的，选择合适的保险产品搭配，清晰告知每份保险的保障能力；\n✔当保险出险时，您可以第一时间联系妈咪研究院，我们会全程协助您进行理赔。")
+            that.data.drtailsClauseContent.push("托管服务包含了全年咨询服务。在一年服务有效期间，您可以通过妈咪研究院的微信公众号，随时随地向我们咨询任何保险相关问题，不限数量，不限主题。")
+            break;
+          default: break;
     }
     that.setData({
       serviceName:that.data.serviceName,
@@ -65,5 +95,99 @@ Page({
     that.setData({
       isAgreeProtocol:!that.data.isAgreeProtocol
     })
+  },
+  pay: function(){
+    var that = this
+    if(that.data.isAgreeProtocol == true){
+      //同意协议
+      wx.login({
+        success: function(res) {
+        //获取用户code，转发到我们的服务器，再在我们服务器与微信交互，获取openid
+        var code = res.code
+        wx.request({
+          url:'https://baby.mamid.cn/Api/Index/paylogin',
+          method: 'POST',
+          data: {
+          'code': code,                       
+          },
+          header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+          success:function(response) {
+            //console.log(response.data)
+            var openid=response.data
+            var price=0.01;
+            var goods_id=1;
+            var goods_name='托管服务';
+            var quantity=1;
+            wx.request({
+              url:'https://baby.mamid.cn/Api/Test/payJoinfee',
+              method: 'POST',
+              data: {
+                  'openid': openid,
+                  'price':price,
+                  'goods_id':goods_id,
+                  'goods_name':goods_name,
+                  'quantity':quantity,
+              },
+              header: {
+                   "Content-Type": "application/x-www-form-urlencoded"
+              },
+              success: function(res) {
+                console.log(res.data);
+                console.log('调起支付');
+                wx.requestPayment({
+                'timeStamp': res.data.timeStamp,
+                'nonceStr': res.data.nonceStr,
+                'package': res.data.package,
+                'signType': res.data.signType,
+                'paySign': res.data.paySign,
+                'success':function(res){
+                console.log('success');
+                wx.showToast({
+                    title: '支付成功',
+                    icon: 'success',
+                    duration: 3000
+                  });
+                  },
+                'fail':function(res){
+                    console.log('fail');
+                },
+                'complete':function(res){
+                    console.log('complete');
+                        }
+                    });
+                },
+              fail:function(res){
+                  console.log(res.data)
+              }
+            })
+            },
+              failure: function(error) {
+                      console.log(error)
+              },
+            })
+          }
+        })
+    var timestamp = Date.parse(new Date());
+    console.log(timestamp);return false; 
+    wx.requestPayment({
+        'timeStamp': timestamp,
+        'nonceStr': '',
+        'package': '',
+        'signType': 'MD5',
+        'paySign': '',
+        'success':function(res){
+        },
+        'fail':function(res){
+   }
+   })
+    }else{
+      wx.showToast({
+        title: '用户未同意《服务协议》',
+        icon: 'loading',
+        duration: 2000
+      })
+    }
   }
 })
